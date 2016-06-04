d8846d5cfe341aba5c9bfe53acc9dfa8f45b0be0
ajwells <ajwells@email.wm.edu>
2016-06-04 02:29:47 -0400

    read and write characteristics

--- app/src/bluetooth.js
+++ app/src/bluetooth.js
@@ -49,36 +49,93 @@ function connect(peripheral) {
 };
 
 function connectAll(devices) {
-		var connectedDevices = [];
-		var promises = [];
-		devices.forEach(function(peripheral, index) {
-			peripheral.on('disconnect', function() {
-				console.log(peripheral.advertisement.localName + ' disconnected');
-				connect(peripheral).then(function(peripheral) {
-					console.log('device reconnected');
-				});
+	var connectedDevices = [];
+	var promises = [];
+	devices.forEach(function(peripheral, index) {
+		peripheral.on('disconnect', function() {
+			console.log(peripheral.advertisement.localName + ' disconnected');
+			connect(peripheral).then(function(peripheral) {
+				console.log('device reconnected');
 			});
-			promises.push(connect(peripheral));
 		});
-		return Promise.all(promises);
+		promises.push(connect(peripheral));
+	});
+	return Promise.all(promises);
 };
 
-discoverList = ['EC:44:71:02:29:55'];
-
-	discover(discoverList)
-	.then(connectAll)
-	.then(function(results) {
-		results.forEach(function(peripheral) {
-			peripheral.discoverSomeServicesAndCharacteristics(
-			['0dbb9a2d39de45908eacbfdef650416f'],
-			['3c66211138f84a8ebdd61e0a5f403277'],
+function readChar(peripheral, serviceUUID, charUUID) {
+	return new Promise(function(resolve, reject) {
+		peripheral.discoverSomeServicesAndCharacteristics([serviceUUID], [charUUID],
 			function(error, services, characteristics) {
 				characteristics[0].read(function(error, data) {
 					if (error) {
-						console.log(error);
+						reject(error);
 					} else {
-						console.log(data.toString());
+						resolve(data);
 					}
+				});
+		});
+	});
+};
+
+function writeChar(peripheral, serviceUUID, charUUID, data, withoutResponse) {
+	return new Promise(function(resolve, reject) {
+		peripheral.discoverSomeServicesAndCharacteristics([serviceUUID], [charUUID],
+			function(error, services, characteristics) {
+				var buf = new Buffer(data, 'utf-8');
+				characteristics[0].write(buf, false, function(error) {
+					if (error) {
+						reject(error);
+					} 
+					console.log('write done');
+					resolve('ok');
+				});
+		});
+	});
+};
+
+function notifyChar(peripheral, serviceUUID, charUUID) {
+	return new Promise(function(resolve, reject) {
+		peripheral.discoverSomeServicesAndCharacteristics([serviceUUID], [charUUID],
+			function(error, services, characteristics) {
+				console.log(characteristics[0].notify);
+				characteristics[0].notify(function(error) {
+					if (error) {
+						reject(error);
+					} 
+					characteristic.on('data', function(data, isNotification) {
+						console.log(data.toString());
+					});
+					console.log('subscribed');
+					resolve('ok');
+				});
+		});
+	});
+};
+
+
+var discoverList = ['EC:44:71:02:29:55'];
+var serviceUUID = '0dbb9a2d39de45908eacbfdef650416f';
+var charUUID = '3c66211138f84a8ebdd61e0a5f403277';
+var connectedList = [];
+var serviceUUID2 = 'a3aa7ef1fc73424a83a85c99a905a4d3';
+var charUUID2 = 'b49b1d222ca7448bb98a1185c5328553';
+
+discover(discoverList)
+	.then(connectAll)
+	.then(function(results) {
+		connectedList = results;
+		results.forEach(function(peripheral) {
+			readChar(peripheral, serviceUUID, charUUID).then(function(data) {
+				console.log(data.toString());
+				writeChar(peripheral, serviceUUID2, charUUID2, 'ON', false).then(function(ok) {
+					console.log(ok);
+					readChar(peripheral, serviceUUID2, charUUID2).then(function(data) {
+						console.log(data.toString());
+						/*notifyChar(peripheral, serviceUUID2, charUUID2).then(function(ok) {
+							console.log(ok);
+						});*/
+					});
 				});
 			});
 		});
